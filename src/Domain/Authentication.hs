48b55729c8fdff8370bb8ab04192b588377fd0df
Benjamin <benjamin.guy.thomas@gmail.com>
2022-08-29 07:36:48 +0200

    Add temp auth

--- src/Domain/Authentication.hs
+++ src/Domain/Authentication.hs
@@ -6,7 +6,8 @@ module Domain.Authentication (
     PasswordValidationError (..),
 ) where
 
-import Data.Text (Text)
+import Control.Monad.Except (ExceptT (ExceptT), MonadTrans (lift), runExceptT)
+import Data.Text (Text, unpack)
 import Domain.Validation (lengthLessThan, regexMatch, validate)
 import Text.Regex.PCRE.Heavy (re)
 
@@ -64,3 +65,51 @@ mkPassword =
         , regexMatch [re|[a-z]|] PasswordMustContainLowerCaseError
         , regexMatch [re|[A-Z]|] PasswordMustContainUpperCaseError
         ]
+
+-- TEMP: I'm not sure where the code below should go yet
+
+type VerificationCode = Text
+
+class Monad m => AuthRepo m where
+    addAuth :: Auth -> m (Either RegistrationError VerificationCode)
+
+class Monad m => EmailVerificationNotif m where
+    notifyEmailVerification :: Email -> VerificationCode -> m ()
+
+register :: (AuthRepo m, EmailVerificationNotif m) => Auth -> m (Either RegistrationError ())
+register auth = runExceptT $ do
+    vCode <- ExceptT $ addAuth auth
+    let email = authEmail auth
+    lift $ notifyEmailVerification email vCode
+
+-- TEMP IMPLEMENTATIONS
+
+instance AuthRepo IO where
+    addAuth (Auth email _pass) = do
+        putStrLn $ "adding auth: " <> unpack (rawEmail email)
+        return $ Right "fake verification code"
+
+instance EmailVerificationNotif IO where
+    notifyEmailVerification email vCode =
+        putStrLn $ "Notify " <> unpack (rawEmail email) <> " - " <> unpack vCode
+
+{-
+Testing temporary impls in the REPL
+
+cabal repl>:l Domain.Authentication
+cabal repl>:l ./src/Domain/Authentication.hs
+
+let Right email = mkEmail "user@example.com"
+let Right password = mkPassword "123456789Ab"
+let auth = Auth email password
+register auth
+
+*Domain.Authentication> let Right email = mkEmail "user@example.com"
+*Domain.Authentication> let Right password = mkPassword "123456789Ab"
+*Domain.Authentication> let auth = Auth email password
+*Domain.Authentication> register auth
+adding auth: user@example.com
+Notify user@example.com - fake verification code
+Right ()
+
+-}
