b155c73f8956e138128d92ac61dc5671fdff618b
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-09-23 22:22:23 +0200

    Learn MTL

---
+++ other/learn-mtl/Main.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Monad.Except (
+    MonadError (throwError),
+    MonadTrans (lift),
+ )
+import Control.Monad.Reader (
+    MonadReader (ask),
+    ReaderT (runReaderT),
+ )
+import Data.Text (Text)
+
+type Env = [(Text, Int)] -- (variableName, variableValue)
+
+data Expr
+    = Val Int
+    | Add Expr Expr
+    | Var Text
+    deriving (Show)
+
+eval :: Expr -> ReaderT Env Maybe Int
+eval = \case
+    Val n -> pure n
+    Add x y -> (+) <$> eval x <*> eval y
+    Var str -> do
+        env <- ask
+        lift (lookup str env)
+
+{-
+>>> example1
+Just 8
+ -}
+example1 :: Maybe Int
+example1 =
+    runReaderT
+        (eval (Add (Val 1) (Add (Var "x") (Var "y"))))
+        [("x", 2), ("y", 5)]
+
+example2 :: Maybe Int
+example2 =
+    runReaderT
+        (eval (Add (Var "x") (Var "y")))
+        []
+
+example3 :: Maybe Int
+example3 =
+    runReaderT
+        (eval (Add (Val 2) (Val 3)))
+        []
+
+newtype EvalError
+    = VarNotFound Text
+
+eval2 :: (MonadReader Env m, MonadError EvalError m) => Expr -> m Int
+eval2 = \case
+    Val n -> pure n
+    Add x y -> (+) <$> eval2 x <*> eval2 y
+    Var str -> do
+        env <- ask
+        case lookup str env of
+            Just n -> pure n
+            Nothing -> throwError $ VarNotFound str
+
+example4 :: Either EvalError Int
+example4 =
+    runReaderT
+        (eval2 (Add (Var "x") (Var "y")))
+        [("x", 2), ("y", 5)]
+
+main :: IO ()
+main = putStrLn "Hello learn-mtl"
