a967c73026cdb1cb934857dce5f2ccb381df2d69
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-09-24 06:26:30 +0200

    Use asks

--- other/learn-mtl/Main.hs
+++ other/learn-mtl/Main.hs
@@ -11,6 +11,7 @@ import Control.Monad.Except (
 import Control.Monad.Reader (
     MonadReader (ask),
     ReaderT (runReaderT),
+    asks,
  )
 import Data.Text (Text)
 
@@ -52,24 +53,42 @@ example3 =
         (eval (Add (Val 2) (Val 3)))
         []
 
+newtype Env2
+    = Env2 {unEnv :: [(Text, Int)]}
+
 newtype EvalError
     = VarNotFound Text
+    deriving (Show)
 
-eval2 :: (MonadReader Env m, MonadError EvalError m) => Expr -> m Int
+eval2 :: (MonadReader Env2 m, MonadError EvalError m) => Expr -> m Int
 eval2 = \case
     Val n -> pure n
     Add x y -> (+) <$> eval2 x <*> eval2 y
     Var str -> do
-        env <- ask
+        env <- asks unEnv
         case lookup str env of
             Just n -> pure n
             Nothing -> throwError $ VarNotFound str
 
+{-
+>>> example4
+Right 7
+ -}
 example4 :: Either EvalError Int
 example4 =
     runReaderT
         (eval2 (Add (Var "x") (Var "y")))
-        [("x", 2), ("y", 5)]
+        (Env2 [("x", 2), ("y", 5)])
+
+{-
+>>> example5
+Left (VarNotFound "y")
+ -}
+example5 :: Either EvalError Int
+example5 =
+    runReaderT
+        (eval2 (Add (Var "x") (Var "y")))
+        (Env2 [("x", 2)])
 
 main :: IO ()
 main = putStrLn "Hello learn-mtl"
